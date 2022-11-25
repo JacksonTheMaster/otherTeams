e409e560736201b57830500f234b3a64a0ed0db3
JacksonTheMaster <81807824+JacksonTheMaster@users.noreply.github.com>
2022-11-25 16:20:55 +0100

    build version

--- start.ps1
+++ start.ps1
@@ -1,3 +1,11 @@
-Write-Host "_________ ......Starting ......_________" -f Magenta
-Write-Host $teams_root_filepath
+$credspath = $Env:Programfiles+"\teamsothertenant\creds.xml"
+$creds =import-clixml -path $credspath
+Write-Host "_________ ....creds imprte ......_________" -f Yellow
+Write-Host $creds.UserName
+Write-Host $creds.Password
+$daemonuser = $creds.UserName
+$daemonpasswd = $creds.Password
+$daemoncreds = New-Object System.Management.Automation.PSCredential $daemonuser, $daemonpasswd
+$teams_root_filepath = "C:\Users\"+$daemonuser+"\AppData\Local\Microsoft\Teams\current\Teams.exe"
+Write-Host "_________ ......Starting teams from [$teams_root_filepath]......_________" -f Magenta
 Start-Process $teams_root_filepath -Credential $daemoncreds
