0aa49cb731cd1a32367f5575525ff3740c16a4fe
JacksonTheMaster <81807824+JacksonTheMaster@users.noreply.github.com>
2022-11-26 09:22:57 +0100

    make start.ps1 actually start the process

--- start.ps1
+++ start.ps1
@@ -8,4 +8,4 @@ $daemonpasswd = $creds.Password
 $daemoncreds = New-Object System.Management.Automation.PSCredential $daemonuser, $daemonpasswd
 $teams_root_filepath = "C:\Users\"+$daemonuser+"\AppData\Local\Microsoft\Teams\current\Teams.exe"
 Write-Host "_________ ......Starting teams from [$teams_root_filepath]......_________" -f Magenta
-#Start-Process $teams_root_filepath -Credential $daemoncreds
+Start-Process $teams_root_filepath -Credential $daemoncreds
